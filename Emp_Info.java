96a0c7dec30e2e37607222f0cc7174c2215bdecd
aherpankaj01 <aherpankaj22@gmail.com>
2026-01-06 16:56:00 +0530

    first commit java

---
+++ Emp_Info.java
@@ -0,0 +1,13 @@
+class Emp_Info 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Emp Name: Vijay");
+		System.out.println("Emp dept: Dev ");
+		System.out.println("Emp dept no: 20");
+		System.out.println("Emp's manager name: Raj");
+		System.out.println("Emp's Sal : 5000");
+		System.out.println("Emp's city: Pune");
+		
+	}
+}
